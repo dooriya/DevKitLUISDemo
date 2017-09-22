e18a20f6d3e2b62f8f58ada163141f0a96ed3b51
Yiri Wen <yirwen@microsoft.com>
2017-09-22 16:54:30 +0800

    .

--- Device_WebSocket/ChatBotDemo_websocket/ChatBotDemo_websocket.ino
+++ Device_WebSocket/ChatBotDemo_websocket/ChatBotDemo_websocket.ino
@@ -16,15 +16,16 @@ void InitWiFi()
 }
 AudioClass& Audio = AudioClass::getInstance();
 const int RING_BUFFER_SIZE = 15000;
-const int playChunk = 4096;
+RingBuffer ringBuffer(RING_BUFFER_SIZE);
 char readBuffer[2048];
+char websocketBuffer[5000];
 bool startPlay = false;
 static char emptyAudio[256];
-RingBuffer ringBuffer(RING_BUFFER_SIZE);
-
+Websocket *websocket;
 void play()
 {
   printf("start play\r\n");
+  Audio.attachRecord(NULL);
   Audio.attachPlay(playCallback);
   Audio.format(8000, 16);
   Audio.startPlay();
@@ -33,27 +34,30 @@ void play()
 
 void record()
 {   
-    ringBuffer.clear();
-    Audio.format(8000, 16);
-    Audio.attachRecord(recordCallback);
-    Audio.startRecord(NULL, NULL, 0);
-}
-
-void playCallback(void)
-{
-    if (ringBuffer.use() < 256)
-    {
-        Audio.write(emptyAudio, 256);
-        return;
-    }
-    ringBuffer.get((uint8_t*)readBuffer, 256);
-    Audio.write(readBuffer, 256);
+  ringBuffer.clear();
+  Audio.format(8000, 16);
+  Audio.attachPlay(NULL);
+  Audio.attachRecord(recordCallback);
+  Audio.startRecord(NULL, NULL, 0);
 }
 
 void stop()
 {
-    Audio.stop();
-    startPlay= false;
+  Audio.stop();
+  Audio.attachRecord(NULL);
+  Audio.attachPlay(NULL);
+  startPlay= false;
+}
+
+void playCallback(void)
+{
+  if (ringBuffer.use() < 256)
+  {
+    Audio.write(emptyAudio, 256);
+    return;
+  }
+  ringBuffer.get((uint8_t*)readBuffer, 256);
+  Audio.write(readBuffer, 256);
 }
 
 void recordCallback(void)
@@ -66,7 +70,7 @@ void setResponseBodyCallback(const char* data, size_t dataSize)
 {
   while(ringBuffer.available() < dataSize) {
     printf("ringBuffer ava %d\r\n", ringBuffer.available());
-    delay(100);
+    delay(10);
   }
   ringBuffer.put((uint8_t*)data, dataSize);
   if (ringBuffer.use() > RING_BUFFER_SIZE / 2 && startPlay == false) {
@@ -74,71 +78,70 @@ void setResponseBodyCallback(const char* data, size_t dataSize)
   }
 }
 
+char* getUrl()
+{
+  char *url;
+  url = (char *)malloc(300);
+  if (url == NULL)
+  {
+    return NULL;
+  }
+  HTTPClient guidRequest = HTTPClient(HTTP_GET, "http://www.fileformat.info/tool/guid.htm?count=1&format=text&hyphen=true");
+  const Http_Response* _response = guidRequest.send();
+  if (_response == NULL)
+  {
+    printf("Guid generator HTTP request failed.\r\n");
+    return NULL;
+  }
+  snprintf(url, 300, "%s%s", "ws://yiribot.azurewebsites.net/chat?nickName=", _response->body);
+  printf("url: %s\r\n", url);
+  return url;
+}
+
 void setup() {
-  
   pinMode(USER_BUTTON_A, INPUT);
   if (WiFi.begin() == WL_CONNECTED)
   {
     printf("connected\r\n");
   }
 
-  char url[300] = "";
-    HTTPClient guidRequest = HTTPClient(HTTP_GET, "http://www.fileformat.info/tool/guid.htm?count=1&format=text&hyphen=true");
-    const Http_Response* _response = guidRequest.send();
-    if (_response == NULL)
-    {
-        printf("Guid generator HTTP request failed.\r\n");
-      return;
-    }
-    snprintf(url, 300, "%s%s", "ws://yiribot.azurewebsites.net/chat?nickName=", _response->body);
-    printf("%s\r\n", url);
-    
-  char *s;
-  s = (char *)malloc(5000);
-  if (s == NULL)
-  {
-      printf("s is null\r\n");  
-  }
   memset(emptyAudio, 0x0, 256);
-  Websocket ws(url);
-  int connect_state = ws.connect();
+  char *url = getUrl();
+  websocket = new Websocket(url);
+  int connect_state = (*websocket).connect();
   printf("connect_state %d\r\n", connect_state);
-  
-while(true){
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
   printf("you can start a new question now\r\n");
-  ws.send("pcmstart", 4, 0x02);
+  (*websocket).send("pcmstart", 4, 0x02);
   while(digitalRead(USER_BUTTON_A) == HIGH) delay(10);
   record();
   while(digitalRead(USER_BUTTON_A) == LOW || ringBuffer.use() > 0)
   {
     if (digitalRead(USER_BUTTON_A) == HIGH) stop();
-    int sz = ringBuffer.get((uint8_t*)s, 2048);
+    int sz = ringBuffer.get((uint8_t*)websocketBuffer, 2048);
     if (sz > 0) {
-      ws.send(s, sz, 0x00);
+      (*websocket).send(websocketBuffer, sz, 0x00);
     }
   }
-  ws.send("pcmend", 4, 0x80);
+  (*websocket).send("pcmend", 4, 0x80);
   delay(100);
   printf("your question send\r\n");
-    ringBuffer.clear();
-    unsigned char opcode = 0;
-    int len = 0;
-    bool first = true;
-    while ((opcode & 0x80) == 0x00) {
-      int tmp = ws.read(s, &len, &opcode, first);
-      printf("tmp %d recv len %d opcode %d\r\n", tmp, len, opcode);
-      first = false;
-      if (tmp == 0) break;
-      setResponseBodyCallback(s, len);
-    }
-    if (startPlay == false) play();
-    while(ringBuffer.use() >= 256) delay(100);
-    ringBuffer.clear();
-    stop();
- }
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+  ringBuffer.clear();
+  
+  unsigned char opcode = 0;
+  int len = 0;
+  bool first = true;
+  while ((opcode & 0x80) == 0x00) {
+    int tmp = (*websocket).read(websocketBuffer, &len, &opcode, first);
+    printf("tmp %d recv len %d opcode %d\r\n", tmp, len, opcode);
+    first = false;
+    if (tmp == 0) break;
+    setResponseBodyCallback(websocketBuffer, len);
+  }
+  if (startPlay == false) play();
+  while(ringBuffer.use() >= 256) delay(100);
+  stop();
 }
